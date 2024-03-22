1271e0f8219e7ab95c455043928076115e2f2df1
Abhishek Ranjan <119099659+abhi-2029@users.noreply.github.com>
2024-03-22 23:47:14 +0530

    Add files via upload

---
+++ Diamond_pattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the value of num: ";
+    cin>>num;
+    int n=num/2;
+        for(int row=0; row<n; row=row+1){
+        for(int col=0; col<n-row-1; col=col+1){
+            cout<<" ";
+        }
+        for(int col=0; col<row+1; col=col+1){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }    for(int row=0; row<n; row=row+1){
+        for(int col=0; col<row; col=col+1){
+            cout<<" ";
+        }
+        for(int col=0; col<n-row; col=col+1){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
